f49799942053941125bae641c34f628d03935f92
jenniferwvng <83246937+jenniferwvng@users.noreply.github.com>
2022-08-18 13:58:16 +0200

    css styles modified

--- src/routes/ConfirmationPage.js
+++ src/routes/ConfirmationPage.js
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { orderConfirmation } from "../features/cart/cartSlice";
 import { restartOrder, emptyCart } from "../features/cart/cartSlice";
 import Header from "../components/Header";
+import styles from '../styles/Confirmation.module.css'
 
 function ConfirmationPage () {
   const order = useSelector(orderConfirmation);
@@ -18,8 +19,8 @@ function ConfirmationPage () {
   }, 8000);
 
   const renderOrderInfo = order.map(orderinfo => (
-    <div>
-        <p>Thank you for your order, {orderinfo.firstname.toUpperCase()}</p>
+    <div className={styles.orderInformation}>
+        <p>Thank you for your order, {orderinfo.firstname}</p>
         <p>You will receive an order confirmation to {orderinfo.email}</p>
         <p>Chosen payment method: {orderinfo.payment}</p>
     </div>
@@ -29,12 +30,17 @@ function ConfirmationPage () {
   return (
     <div>
       <Header />
-      <div style={{textAlign: 'center'}}>
-        {renderOrderInfo}      
-        <Link to="/">
-          Return to start page
-        </Link>
-        <p>You will be automatically redirected after 8 seconds</p>
+      <div className={styles.displayGrid}>
+        <div className={styles.gridChild}>
+        {renderOrderInfo}    
+          <span>
+          <button className={styles.redirectButtonParent}>
+            <Link to="/" className={styles.redirectButton}>
+              Return to start page (you will be redirected after 8 seconds)
+            </Link>
+          </button>
+          </span>  
+        </div>
       </div>
     </div>
   );
